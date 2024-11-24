97064ad43c04c4c5a6e039e91dab88f5b43e3536
MichaelJGKopp <michaeljg.kopp@gmail.com>
2024-11-24 12:31:27 +0100

    use application.properties for server port & variable injection

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,7 +1,22 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FunRestController {
 
+  @Value("${coach.name}")
+  private String coachName;
 
+  @Value("${team.name}")
+  private String teamName;
 
+  // expose "/" that return "Hello World"
+  // localhost:8080
+  @GetMapping("/")
+  public String sayHello() {
+    return "Hello World" + " by " + coachName + " from: " + teamName;
+  }
 }
