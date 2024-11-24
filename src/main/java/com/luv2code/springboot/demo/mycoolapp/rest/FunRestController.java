9dc9d8c29f54253272b4a36491ed1d83cffb20f6
MichaelJGKopp <michaeljg.kopp@gmail.com>
2024-11-24 14:44:19 +0100

    expose new RestController endpoints

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -15,8 +15,23 @@ public class FunRestController {
 
   // expose "/" that return "Hello World"
   // localhost:8080
+
   @GetMapping("/")
   public String sayHello() {
     return "Hello World" + " by " + coachName + " from: " + teamName;
+  }
+
+  // expose a new endpoint for "workout"
+
+  @GetMapping("/workout")
+  public String getDailyWorkout() {
+    return "Run a hard 5k!";
+  }
+
+  // expose a new endpoint for "fortune"
+
+  @GetMapping("/fortune")
+  public String getDailyFortune(){
+    return "Today is your lucky day.";
   }
 }
